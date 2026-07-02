23e284a9804174c2ee60b43e1ea57b887df28fd1
AYadrov <kowalski228@mail.ru>
2026-07-02 14:59:53 -0600

    little fix

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -17,12 +17,12 @@ make install
 xz -d -k -f infra/points.json.xz
 xz -d -k -f infra/optimal_precisions.json.xz
 racket -y infra/time.rkt --dir "$REPORTDIR" --profile profile.json --optimal-precisions infra/optimal_precisions.json infra/points.json
-python infra/ratio_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
-python infra/point_graph.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
-python infra/histograms.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
-python infra/cnt_per_iters_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
-python infra/repeats_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
-python infra/density_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
-python infra/optimality_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/ratio_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/point_graph.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/histograms.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/cnt_per_iters_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/repeats_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/density_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
+python3 infra/optimality_plot.py -t "$REPORTDIR"/timeline.json -o "$REPORTDIR"
 cp profile.json "$REPORTDIR"/profile.json
 cp infra/profile.js "$REPORTDIR"/profile.js
