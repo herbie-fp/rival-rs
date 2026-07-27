8b704fe5d223724f86739ff21b875c9480624068
Aditya Kumar <obround.dev@gmail.com>
2026-07-27 00:39:32 -0700

    Add arm64-linux build

--- rival3-racket/info.rkt
+++ rival3-racket/info.rkt
@@ -5,6 +5,12 @@
 
 (define pkg-desc "Racket bindings to Rival 3")
 (define license 'MIT)
+(define build-platforms
+  '("win32-x86_64"
+    "linux-x86_64"
+    "linux-aarch64"
+    "macosx-x86_64"
+    "macosx-aarch64"))
 
 (define deps '(("base" #:version "8.0") "math-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "math-doc"))
